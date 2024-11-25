b23839e7bb19f5b4d14c9de239a20b68614087cb
Arthur <76011758+aboivert@users.noreply.github.com>
2024-11-25 15:28:04 +0100

    Update fusion_perlees_app.py

--- fusion_perlees_app.py
+++ fusion_perlees_app.py
@@ -127,7 +127,7 @@ if fusions_file is not None:
                             get_color="[255,255, 0]",
                             pickable=True,
                             auto_highlight=True,
-                            get_radius=50000,
+                            get_radius=500,
                         )
                         view_state = pydeck.ViewState(
                             latitude=temp_data.stop_lat.mean(), longitude=temp_data.stop_lon.mean(), controller=True, zoom=5 , pitch=30
